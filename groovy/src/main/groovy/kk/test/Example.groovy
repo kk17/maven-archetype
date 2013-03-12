24531ce8df20f5c0fd80d96a0950dc1e5546bcbc
kk17 <zk.chan007@gmail.com>
2013-03-13 00:23:23 +0800

    add groovy and quick-start project

---
+++ groovy/src/main/groovy/kk/test/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package kk.test
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
